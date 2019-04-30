5602422e3057592ce79446b6442f270c52c2f0b3
Amir Alavi <s.amir.alavi@gmail.com>
2019-04-29 22:48:19 -0400

    ICP pulled out into module, ScAlign added

---
+++ icp.py
@@ -0,0 +1,167 @@
+from collections import defaultdict
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+from IPython import display
+
+# Code for ICP methods
+def isnan(x):
+    return x != x
+
+def get_rigid_transformer(ndims, bias=False):
+    model = nn.Sequential(nn.Linear(ndims, ndims, bias=bias))
+    print(model[0].weight.data.dtype)
+    model[0].weight.data.copy_(torch.eye(ndims))#, dtype=torch.double))
+    print(model[0].weight.data.dtype)
+    return model
+
+def closest_point_loss(A, B):
+    loss = A.unsqueeze(1) - B.unsqueeze(0)
+    loss = loss**2
+    loss = loss.mean(dim=-1)
+    loss, matches = loss.min(dim=1)
+    unique_matches = np.unique(matches.numpy())
+    loss = loss.sum()
+    return loss, unique_matches
+
+def closest_point_loss_ignore(A, B):
+    # build distance matrix
+    loss = A.unsqueeze(1) - B.unsqueeze(0)
+    loss = loss**2
+    loss = loss.mean(dim=-1)
+    # Select pairs, iteratively building up a mask
+    mask = np.zeros(loss.shape, dtype=np.float32)
+    sorted_idx = np.stack(np.unravel_index(np.argsort(loss.detach().numpy().ravel()), loss.shape), axis=1)
+    target_matched = set()
+    source_matched = set()
+    matched = 0
+    for i in range(sorted_idx.shape[0]):
+#         if matched >= 100:
+#             break
+        match_idx = sorted_idx[i]
+        #if match_idx[0] not in source_matched and match_idx[1] not in target_matched:
+        if match_idx[1] not in target_matched:
+            mask[match_idx[0], match_idx[1]] = 1
+            matched += 1
+            target_matched.add(match_idx[1])
+            source_matched.add(match_idx[0])
+    mask = torch.from_numpy(mask)
+    #print(mask.sum())
+    loss = torch.mul(loss, mask).sum()
+    loss = loss.sum()
+    #print(target_matched)
+    #sys.exit()
+    return loss, np.array(list(target_matched))
+
+def relaxed_match_loss(A, B):
+    # build distance matrix
+    loss = A.unsqueeze(1) - B.unsqueeze(0)
+    loss = loss**2
+    loss = loss.mean(dim=-1)
+    # Select pairs, iteratively building up a mask
+    mask = np.zeros(loss.shape, dtype=np.float32)
+    sorted_idx = np.stack(np.unravel_index(np.argsort(loss.detach().numpy().ravel()), loss.shape), axis=1)
+    target_matched_counts = defaultdict(int)
+    source_matched = set()
+    matched = 0
+    for i in range(sorted_idx.shape[0]):
+#         if matched >= 100:
+#             break
+        match_idx = sorted_idx[i]
+        #if match_idx[0] not in source_matched and match_idx[1] not in target_matched:
+        if target_matched_counts[match_idx[1]] < 2 and match_idx[0] not in source_matched:
+            target_matched_counts[match_idx[1]] += 1
+            mask[match_idx[0], match_idx[1]] = 1
+            source_matched.add(match_idx[0])
+    mask = torch.from_numpy(mask)
+    #print(mask.sum())
+    loss = torch.mul(loss, mask).sum()
+    loss = loss.sum()
+    #print(target_matched)
+    #sys.exit()
+    return loss, np.array(list(target_matched_counts.keys()))
+
+def plot_step(A, B, type_index_dict, pca, step, matched_targets, closest_points, losses):
+    A = pca.transform(A)
+    B = pca.transform(B)
+    mask = np.zeros(B.shape[0], dtype=bool)
+    mask[matched_targets] = True
+    B_matched = B[mask]
+    B_unmatched = B[~mask]
+    fig, axes = plt.subplots(2, 2, figsize=(20,20))
+    # Scatter, colored by dataset, with matching
+    axes[0,0].scatter(A[:,0], A[:,1], c='m', label='source', alpha=0.15)
+    axes[0,0].scatter(B_unmatched[:,0], B_unmatched[:,1], c='b', label='target', alpha=0.15)
+    axes[0,0].scatter(B_matched[:,0], B_matched[:,1], c='g', label='matched target', alpha=0.75)
+    axes[0,0].legend()
+    # Training loss & number of target matches
+    axes[0,1].plot(closest_points, label='matches')
+    axes[0,1].set_xlabel('iteration')
+    axes[0,1].set_ylabel('matches')
+    axes[0,1].set_title('num unique matches in target set')
+    axes[0,1].legend(loc='upper left')
+    ax_matches = axes[0,1].twinx()
+    ax_matches.set_ylabel('loss')
+    ax_matches.plot(losses, c='orange', label='loss')
+    ax_matches.legend(loc='upper right')
+    # Scatter, colored by dataset
+    axes[1,0].scatter(A[:,0], A[:,1], c='m', label='source', alpha=0.15)
+    axes[1,0].scatter(B[:,0], B[:,1], c='b', label='target', alpha=0.15)
+    axes[1,0].legend()
+    # Scatter, colored by cell types
+    combined = np.concatenate((A, B))
+    for cell_type, idx in type_index_dict.items():
+        axes[1,1].scatter(combined[idx, 0], combined[idx, 1], label=cell_type, alpha=0.15)
+    axes[1,1].legend()
+    display.clear_output(wait=True)
+    display.display(plt.gcf())
+    plt.close()
+    #time.sleep(0.5)
+
+def ICP(A, B, type_index_dict,
+        loss_function,
+        max_iters=100,
+        sgd_steps=100,
+        tolerance=1e-4,
+        standardize=True):
+    #A, B = shift_CoM(A, B)
+    if standardize:
+        scaler = StandardScaler().fit(np.concatenate((A, B)))
+        A = scaler.transform(A)
+        B = scaler.transform(B)
+    combined = np.concatenate((A, B))
+    pca = PCA(n_components=2).fit(combined)
+    A = torch.from_numpy(A).float()
+    B = torch.from_numpy(B).float()
+    assert(not isnan(A).any() and not isnan(B).any())
+    transformer = get_rigid_transformer(A.shape[1])
+    optimizer = optim.SGD(transformer.parameters(), lr=1e-3)
+    transformer.train()
+    hit_nan = False
+    num_matches = []
+    losses = []
+    for i in range(max_iters):
+        if hit_nan:
+            break
+        try:
+            A_transformed = transformer(A)
+            if isnan(A_transformed).any():
+                print('encountered NaNs')
+                print(transformer[0].weight.data)
+                hit_nan = True
+                break
+            optimizer.zero_grad()
+            loss, unique_matches = loss_function(A_transformed, B)
+            losses.append(loss.item())
+            num_matches.append(len(unique_matches))
+            plot_step(A_transformed.detach().numpy(), B.detach().numpy(), type_index_dict, pca, i, unique_matches, num_matches, losses)
+            loss.backward()
+            optimizer.step()
+        except KeyboardInterrupt:
+            break
+    return transformer
