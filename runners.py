26a9f763dc253452371c7952d3c3b1f692ec2ad1
Amir Alavi <aalavi@andrew.cmu.edu>
2020-03-15 19:36:50 -0400

    Removed printing model

--- runners.py
+++ runners.py
@@ -96,7 +96,6 @@ def run_ICP_stacked_aes(datasets, task, task_adata, method_name, log_dir, args):
                                     target_match_limit=args.target_match_limit)
     
     scipr.fit(A, B)
-    print(scipr.autoencoders_)
     with open(log_dir / 'scipr_model.pkl', 'wb') as f:
         pickle.dump(scipr, f)
     A, B, type_index_dict, combined_meta = alignment_task.get_source_target(datasets, task, use_PCA=args.input_space == 'PCA', subsample=False)
